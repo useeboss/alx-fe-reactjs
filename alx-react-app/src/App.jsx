d2217f59164ba10a090b30fc8d757a8a8ebc82d6
Usman Yahaya <yahayausman5@gmail.com>
2025-11-30 22:12:24 +0100

    added WelcomeMessage.jsk

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WelcomeMessage from './components/WelcomeMessage';
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,6 +26,15 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        function App() {
+  return (
+    <div>
+      <WelcomeMessage />
+    </div>
+  );
+}
+
+export default App;
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
